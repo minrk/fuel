b4cb1766840e0106ad24c0f32ccf220310092352
David Warde-Farley <wardefar@iro.umontreal.ca>
2015-04-24 15:14:54 -0400

    Switch to PUB/SUB/REQ/REP.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -66,8 +66,9 @@ class DummyWorker(DivideAndConquerWorker):
 
 
 class DummySink(DivideAndConquerSink):
-    def __init__(self, result_port):
+    def __init__(self, result_port, sync_port):
         self.result_port = result_port
+        self.sync_port = sync_port
         self.messages_received = 0
         self.sum = 0
 
@@ -80,33 +81,50 @@ class DummySink(DivideAndConquerSink):
 
     def setup_sockets(self, context, *args, **kwargs):
         super(DummySink, self).setup_sockets(context, *args, **kwargs)
-        self.result_socket = context.socket(zmq.PUSH)
-        self.result_socket.connect('tcp://localhost:{}'.format(
-            self.result_port))
+        self.result_socket = context.socket(zmq.PUB)
+        self.result_socket.bind('tcp://*:{}'.format(self.result_port))
+        self.result_sync = context.socket(zmq.REP)
+        self.result_sync.bind('tcp://*:{}'.format(self.sync_port))
 
     def process(self, number_squared):
         self.sum += number_squared
 
     def shutdown(self):
+        self.result_sync.recv()
+        print('Received sync packet (in sink)')
+        print('Sending sync reply (in sink)')
+        self.result_sync.send(b'')
+        print('Sent sync reply (in sink)')
         print('sending', self.sum)
         self.result_socket.send_pyobj(self.sum)
 
 
 def test_localhost_divide_and_conquer_manager():
-    result_port = 59591
-    ventilator_port = 59592
-    sink_port = 59593
+    result_port = 59581
+    sync_port = 59582
+    ventilator_port = 59583
+    sink_port = 59584
     manager = LocalhostDivideAndConquerManager(DummyVentilator(),
-                                               DummySink(result_port),
+                                               DummySink(result_port,
+                                                         sync_port),
                                                [DummyWorker(), DummyWorker()],
                                                ventilator_port, sink_port)
     context = zmq.Context()
-    result_socket = context.socket(zmq.PULL)
-    result_socket.bind('tcp://*:{}'.format(result_port))
     manager.launch()
-    manager.wait_for_sink()
+    result_socket = context.socket(zmq.SUB)
+    result_socket.connect('tcp://localhost:{}'.format(result_port))
+    sync_socket = context.socket(zmq.REQ)
+    sync_socket.connect('tcp://localhost:{}'.format(sync_port))
+    print('Sending sync packet (in test)')
+    sync_socket.send(b'')
+    print('Sent sync packet (in test)')
+    print('Receiving sync reply (in test)')
+    sync_socket.recv()
+    print('Got sync reply (in test)')
+    print('Receiving message (in test)')
     result = result_socket.recv_pyobj()
-    print("Received", result)
+    print("Received", result, '(in test)')
+    manager.wait_for_sink()
     assert result == sum(i ** 2 for i in range(50))
 
 if __name__ == "__main__":
